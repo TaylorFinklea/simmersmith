ad20e2c8129b9a72705ee995259ccda2203dbbb2
Taylor Finklea <taylor.finklea@gmail.com>
2026-07-25 16:48:14 -0500

    chore(release): bump to build 167 [skip ci]

--- SimmerSmith/SimmerSmith/Features/ReleaseNotes/ReleaseNotesCatalog.swift
+++ SimmerSmith/SimmerSmith/Features/ReleaseNotes/ReleaseNotesCatalog.swift
@@ -20,6 +20,15 @@ enum ReleaseNotesCatalog {
 
     static let all: [ReleaseNote] = [
         ReleaseNote(
+            build: 167,
+            version: "1.0.0",
+            date: "July 25, 2026",
+            headline: "Under the hood",
+            new: [],
+            improved: [],
+            fixed: []
+        ),
+        ReleaseNote(
             build: 166,
             version: "1.0.0",
             date: "July 25, 2026",
